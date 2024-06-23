d504d314208055dc2889e54abaf4cd115f0fcdb8
Ricardo Ortega Pacheco <116306561+ricardoortega97@users.noreply.github.com>
2024-06-23 01:56:01 +0000

    "Session 3_1 updated"

--- Session3_1.py
+++ Session3_1.py
@@ -16,3 +16,86 @@ def swap_ends(my_str):
 my_str = "boast"
 swapped = swap_ends(my_str)
 print(swapped)
+
+#Is pangram
+def is_pangram(my_str):
+      alpha = 'abcdefghijklmopqrstuvwxyz'
+      dict = {char: 0 for char in alpha}
+
+      my_str = my_str.lower()
+
+      for i in my_str:
+        if i in dict:
+          dict[i] = dict[i] + 1
+
+      for val in dict.values():
+        if val == 0:
+          return False 
+        else:
+          return True 
+
+my_str = "The quick brown fox jumps over the lazy dog"
+print(is_pangram(my_str))
+str2 = "The dog jumped"
+print(is_pangram(str2))
+
+#reverse String
+def reverse_string(my_str):
+  my_str = my_str[::-1]
+  return my_str
+
+my_str = "live"
+print(reverse_string(my_str))
+
+def first_unique_char(my_str):
+  strings = {}
+  for char in my_str:
+    if char not in strings:
+      strings[char] = 1
+    else:
+      strings[char] += 1
+
+  for index, char in enumerate(my_str):
+    if strings[char] == 1:
+      return index
+
+  return -1
+
+
+my_str = "leetcode"
+print(first_unique_char(my_str))
+
+str2 = "loveleetcode"
+print(first_unique_char(str2))
+
+str3 = "aabb"
+print(first_unique_char(str3))
+
+#Distances 
+def min_distance(words, word1, word2):
+  index1, index2 = -1, -1
+  min_dis = len(words)
+  #print("len", min_dis)
+  for i, word in enumerate(words):
+    if word == word1:
+      print(word)
+      index1 = i
+      if index2 != -1:
+        min_dis = min(min_dis, index1 - index2)
+    elif word == word2:
+        index2 = i 
+        if index1 != -1:
+          min_dis = min(min_dis, index2 - index1)
+
+  #print("distace" ,min_dis)
+  return min_dis if min_dis != len(words) else -1
+
+words = ["the", "quick", "brown", "fox", "jumped", "the"]
+dist1 = min_distance(words, "quick", "jumped")
+dist2 = min_distance(words, "the", "jumped")
+print(dist1)
+print(dist2)
+
+words2 = ["code", "path", "code", "contribute",  "practice"]
+dist3 = min_distance(words2, "code", "practice")
+print(dist3)
---
+++ Session3_2.py
@@ -0,0 +1,121 @@
+#Sum of String 
+def sum_of_number_strings(nums):
+  sum = 0
+  for num in nums:
+    sum += int(num)
+
+  return sum
+
+nums = ["10", "20", "30"]
+sum = sum_of_number_strings(nums)
+print(sum)
+
+#remove 
+def remove_duplicates(nums):
+  count = 1
+  while count < len(nums):
+    if nums[count] == nums[count -1]:
+      nums.pop(count)
+    else:
+      count += 1
+
+  return nums
+
+nums = [1,1,1,2,3,4,4,5,6,6]
+print(remove_duplicates(nums))
+
+#Reverse Letters
+def reverse_only_letters(s):
+  #size of the list
+  back = len(s)-1
+  newList =[]
+
+  #for loop: 0 to end of list
+  for index in range(0,back+1):
+  #varibale holds front index and back index
+    if(not s[index].isalpha()):
+       newList.append(s[index])
+
+    if(s[back].isalpha()):
+      newList.append(s[back])
+
+    back -=1
+
+  return newList 
+  #return the new list
+s = "a-bC-dEf-ghIj"
+reversed_s = reverse_only_letters(s)
+print(reversed_s)
+
+#Longest Uniform Substring
+def longest_uniform_substring(s):
+  if not s:
+      return 0
+  max_length = 1
+  current_length = 1
+  for i in range(1, len(s)):
+      if s[i] == s[i-1]:
+          current_length += 1
+          max_length = max(max_length, current_length)
+      else:
+          current_length = 1
+  return max_length
+
+s1 = "aabbbbCdAA"
+l1 = longest_uniform_substring(s1)
+print(l1)
+
+s2 = "abcdef"
+l2 = longest_uniform_substring(s2)
+print(l2)
+
+#Teemo attack
+def findPoisonedDuration(timeSeries, duration) -> int:
+  if not timeSeries:
+      return 0
+  total = 0 
+  #the last one will not next hit to reset the time therefore gets added last 
+  for time in range(len(timeSeries) - 1):
+      #print(timeSeries[time + 1] - timeSeries[time]) #4 minus 1, 9 minus 4 
+      total += min(duration, timeSeries[time + 1] - timeSeries[time])
+      #print("total", total )
+  return total + duration
+
+timeSeries = [1,4,9,7]
+duration = 3
+
+damage = findPoisonedDuration(timeSeries, duration)
+print(damage) 
+
+def sumOfUniqueElements(lst1, lst2):  
+  unique_sum = 0
+
+  for num in lst1:
+      # Count occurrences of num in lst1
+      count = 0
+      for x in lst1:
+          if x == num:
+              count += 1
+
+      # Check if num is unique in lst1 and not in lst2
+      if count == 1:
+          in_lst2 = False
+          for y in lst2:
+              if y == num:
+                  in_lst2 = True
+                  break
+
+          if not in_lst2:
+              unique_sum += num
+
+  return unique_sum
+
+lstA = [1, 2 ,3, 4] 
+lstB = [3, 4, 5, 6]
+lstC = [7, 7, 7, 7, 1]
+
+sum1 = sumOfUniqueElements(lstA, lstB)
+print(sum1)
+
+sum2 = sumOfUniqueElements(lstC, lstB)
+print(sum2)
