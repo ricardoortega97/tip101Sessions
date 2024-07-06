fdfb624144b382d57fc6159bca33a7a5cd12718b
Ricardo Ortega Pacheco <116306561+ricardoortega97@users.noreply.github.com>
2024-07-06 02:54:58 +0000

    "Pokémon Class oop"

--- main.py
+++ main.py
@@ -1,16 +1,92 @@
-#Duplicates within Range
-def contains_nearby_duplicate(lst, k):
-  dic = {}
+# 5.1 Pokemon Class 
+class Pokemon:
+  def __init__(self, name, types, evolution= None):
+      self.name = name
+      self.types = types
+      self.is_caught = False
+      self.evolution = evolution
+    
+  def print_pokemon(self):
+    print({
+        "name": self.name,   # Output: "name": "Squirtle",
+        "types": self.types, # Output: "types": ["Water"],
+        "is_caught": self.is_caught # Output: "is_caught": False
+    })
 
-  for i, elem in enumerate(lst):
-    print(i, elem)
-    if elem in dic and i - dic[elem] <= k:
-      return True
+  def catch(self):
+    self.is_caught = True
 
-    dic[elem] = i 
-  return False
+  def choose(self):
+    if self.is_caught:
+      print(f"{self.name} is wild! Catch them if you can!")
+    else:
+      print(f"{self.name} I choose you! ")
 
-lst = [1, 2, 3, 1, 2, 3]
-lst2 = [1, 0, 1, 1]
-print(contains_nearby_duplicate(lst, 2))
-print(contains_nearby_duplicate(lst2, 1))
+  def add_type(self, new_type):
+    self.types.append(new_type)
+
+  def __repr__(self) -> str:
+    return self.name
+
+def getByType(myPokemon, pokemonType):
+    haveType = []
+    for pokemon in myPokemon:
+      if pokemonType in pokemon.types:
+        haveType.append(pokemon)
+        
+    return haveType
+
+def get_evolutionary_line(starter_pokemon):
+  evolution = [starter_pokemon]
+
+  currentPokemon = starter_pokemon
+  while currentPokemon.evolution:
+    evolution.append(currentPokemon.evolution)
+    currentPokemon = currentPokemon.evolution
+
+  return evolution
+
+charizard = Pokemon("Charizard", ["fire", "flying"])
+charmeleon = Pokemon("Charmeleon", ["fire"], charizard)
+charmander = Pokemon("Charmander", ["fire"], charmeleon)
+
+charmander_list = get_evolutionary_line(charmander)
+print(charmander_list)
+
+charmeleon_list = get_evolutionary_line(charmeleon)
+print(charmeleon_list)
+
+charizard_list = get_evolutionary_line(charizard)
+print(charizard_list)
+
+
+'''my_pokemon = Pokemon('pikachu', ["Electric"] )
+squirtle = Pokemon("Squirtle", ["Water"])
+squirtle.print_pokemon()
+squirtle.is_caught = True
+
+squirtle.print_pokemon()
+
+my_pokemon = Pokemon("rattata", ["Normal"])
+my_pokemon.print_pokemon()
+
+my_pokemon.choose()
+my_pokemon.catch()
+my_pokemon.choose()
+
+jigglypuff = Pokemon("Jigglypuff", ["Normal"])
+jigglypuff.print_pokemon()
+
+jigglypuff.add_type("Fairy")
+jigglypuff.print_pokemon()
+
+jigglypuff = Pokemon("Jigglypuff", ["Normal", "Fairy"])
+diglett = Pokemon("Diglett", ["Ground"])
+meowth = Pokemon("Meowth", ["Normal"])
+pidgeot = Pokemon("Pidgeot", ["Normal", "Flying"])
+blastoise = Pokemon("Blastoise", ["Water"])
+
+
+myPokemon = [jigglypuff, diglett, meowth, pidgeot, blastoise]
+normal_pokemon = getByType(myPokemon, "Normal")
+print(normal_pokemon)'''
--- session4_2.py
+++ session4_2.py
@@ -126,3 +126,20 @@ s1 = "xyzzaz"
 s2 = "xyzxyz"
 print(count_good_substrings(s1))
 print(count_good_substrings(s2))
+
+#Duplicates within Range
+def contains_nearby_duplicate(lst, k):
+  dic = {}
+
+  for i, elem in enumerate(lst):
+    print(i, elem)
+    if elem in dic and i - dic[elem] <= k:
+      return True
+
+    dic[elem] = i 
+  return False
+
+lst = [1, 2, 3, 1, 2, 3]
+lst2 = [1, 0, 1, 1]
+print(contains_nearby_duplicate(lst, 2))
+print(contains_nearby_duplicate(lst2, 1))
