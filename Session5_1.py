9dbad964381a6e678390eaa116b8fb1919fd5dee
Ricardo Ortega Pacheco <116306561+ricardoortega97@users.noreply.github.com>
2024-07-16 20:54:54 +0000

    'Commit"

--- Session5_1.py
+++ Session5_1.py
@@ -224,3 +224,27 @@ print(node3.value, "->", node3.next.value)
 print(node4.value, "->", node4.next)'''
 
 
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+def chase(head):
+  current = head
+  values = []
+  while current:
+    values.append(current.value)
+    current = current.next
+  print(' chases '.join(values))
+
+
+dog = Node("Spike")
+cat = Node("Tom")
+mouse = Node("Jerry")
+cheese = Node("Gouda")
+
+dog.next = cat
+cat.next = mouse
+mouse.next = cheese
+
+chase(dog)
