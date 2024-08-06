faca01e73634318db9a9dfb36b3bea3c1925ec6e
Ricardo Ortega Pacheco <116306561+ricardoortega97@users.noreply.github.com>
2024-08-06 20:52:55 +0000

    '7.1 practice'

--- main.py
+++ main.py
@@ -1,54 +1,111 @@
-class TreeNode:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+#In the Stars
+print("\n --- Problem 3.1 ---")
+def insertStars(s):
+    if len(s) <= 1:
+      return s
 
-root = TreeNode(1)
-root.right = TreeNode(6)
-root.right.left = TreeNode(3)
+    else:
+      return s[0] + "*" + insertStars(s[1:])
 
-def nodeSumI(root):
-    return root.val == (root.left.val + root.right.val)
+output = insertStars('abcd')
+print(output)
+print("\n --- Problem 3.2 ---")
 
-def nodeSumII(root):
-    #ternary conditional operator 
-    left = root.left.val if root.left else 0 
-    #value is true if condition else false value 
-    right = root.right.val if root.right else 0
+#String Lenght Cases
+def strLength(s):
+  if not s:
+    return 0
 
-    return root.val == (left + right)
-def leftMost(root):
-    #Base case if there is no tree 
-    if not root:
-        return None
+  else:
+    return 1 + strLength(s[1:])
+
+output = strLength('abc')
+print(output)
+print("\n --- Problem 3.3 ---")
+#Recursive Digits Sum 
+def sumDigits(n):
+  if n == 0:
+    return 0
+  else:
+    return (n % 10) + sumDigits(n // 10)
+
+
+output = sumDigits(12345)
+print(output)
+print("\n --- Problem 3.4 ---")
+
+#Recursive Count 7s
+def countSevens(n):
+  if n == 0:
+    return 0
+  elif n % 10 == 7:
+    return 1 + countSevens(n // 10)
+  else:
+    return countSevens(n // 10)
+n = 727
+output = countSevens(n)
+print(output)
+print("\n --- Problem 3.5 ---")
+
+#Binary Search III
+def binaryIII(lst, target):
+  left, right = 0, len(lst) - 1
+
+  while left < right:
+    mid = (left + right) // 2
+
+    if lst[mid] == target:
+      return True
+    elif lst[mid] > target:
+      right = mid -1
+    else:
+      left = mid + 1
+
+  return False
+
+lst = lst = [1, 3, 5, 7, 9, 11, 13, 15]
+output = binaryIII(lst, 11)
+print(output)
+print("\n --- Problem 3.6 ---")
+#Find Missing
+def find_missing(nums):
+  left, right = 0, len(nums)
+
+  while left < right:
+    mid = (left + right) // 2
+    if nums[mid] > mid:
+      right = mid
+    else:
+      left = mid + 1
+      
+  return left 
+
+nums = [0,1,2,3]
+output = find_missing(nums)
+print(output)
+print("\n --- Problem 3.7 ---")
+#Square Root 
+def sqrt(x):
+  if x < 2:
+    return X
     
-    while root.left:
-        root = root.left
-    return root.val
+  left, right = 1, x // 2
+  while left <= right:
+    mid = (left + right) // 2
+    mid_sqrt = mid * mid
+    if mid_sqrt == x:
+      return mid
+    elif mid_sqrt < x:
+      left = mid + 1
+    else:
+      right = mid - 1
 
-def leftMostRecursive(root):
-    if not root:
-        return None
+  return right
+
+print(sqrt(16))
     
-    if not root.left:
-        return root.val
-        
-    return leftMostRecursive(root.left)
 
-def inorderTraversal(root):
-    values = []
-    return inorderHelper(root, values)
 
-def inorderHelper(currerntNode, values):
-    if not currerntNode:
-        return values
-    inorderHelper(currerntNode.left, values)
-    values.append(currerntNode.val)
-    inorderHelper(currerntNode.right, values)
-    return values
-        
-print(nodeSumII(root))
-print(leftMostRecursive(root))
-print(inorderTraversal(root))
+  
+  
 
--- s7_1.py
+++ s7_1.py
@@ -212,3 +212,109 @@ def ternary(lst, target):
 lst2 = [1, 3, 5, 7, 9, 11, 13, 15]
 output = ternary(lst2, 3)
 print(output)
+
+#In the Stars
+print("\n --- Problem 3.1 ---")
+def insertStars(s):
+    if len(s) <= 1:
+      return s
+
+    else:
+      return s[0] + "*" + insertStars(s[1:])
+
+output = insertStars('abcd')
+print(output)
+print("\n --- Problem 3.2 ---")
+
+#String Lenght Cases
+def strLength(s):
+  if not s:
+    return 0
+
+  else:
+    return 1 + strLength(s[1:])
+
+output = strLength('abc')
+print(output)
+print("\n --- Problem 3.3 ---")
+#Recursive Digits Sum 
+def sumDigits(n):
+  if n == 0:
+    return 0
+  else:
+    return (n % 10) + sumDigits(n // 10)
+
+
+output = sumDigits(12345)
+print(output)
+print("\n --- Problem 3.4 ---")
+
+#Recursive Count 7s
+def countSevens(n):
+  if n == 0:
+    return 0
+  elif n % 10 == 7:
+    return 1 + countSevens(n // 10)
+  else:
+    return countSevens(n // 10)
+n = 727
+output = countSevens(n)
+print(output)
+print("\n --- Problem 3.5 ---")
+
+#Binary Search III
+def binaryIII(lst, target):
+  left, right = 0, len(lst) - 1
+
+  while left < right:
+    mid = (left + right) // 2
+
+    if lst[mid] == target:
+      return True
+    elif lst[mid] > target:
+      right = mid -1
+    else:
+      left = mid + 1
+
+  return False
+
+lst = lst = [1, 3, 5, 7, 9, 11, 13, 15]
+output = binaryIII(lst, 11)
+print(output)
+print("\n --- Problem 3.6 ---")
+#Find Missing
+def find_missing(nums):
+  left, right = 0, len(nums)
+
+  while left < right:
+    mid = (left + right) // 2
+    if nums[mid] > mid:
+      right = mid
+    else:
+      left = mid + 1
+
+  return left 
+
+nums = [0,1,2,3]
+output = find_missing(nums)
+print(output)
+print("\n --- Problem 3.7 ---")
+#Square Root 
+def sqrt(x):
+  if x < 2:
+    return x
+
+  left, right = 1, x // 2
+  while left <= right:
+    mid = (left + right) // 2
+    mid_sqrt = mid * mid
+    if mid_sqrt == x:
+      return mid
+    elif mid_sqrt < x:
+      left = mid + 1
+    else:
+      right = mid - 1
+
+  return right
+
+print(sqrt(16))
