372f8522fb46abead58fb115a4afd808c92c7a6a
Ricardo Ortega Pacheco <116306561+ricardoortega97@users.noreply.github.com>
2024-08-08 23:26:12 +0000

    'More Recursion Practices'

--- main.py
+++ main.py
@@ -1,111 +1,62 @@
-#In the Stars
-print("\n --- Problem 3.1 ---")
-def insertStars(s):
-    if len(s) <= 1:
-      return s
+print('\n --- Problem 1.1 ---')
+#Neatly Nested 
+def isNested(paren):
+  #base case: when the list is empty, it is true 
+  if paren == '':
+    return True
 
-    else:
-      return s[0] + "*" + insertStars(s[1:])
-
-output = insertStars('abcd')
-print(output)
-print("\n --- Problem 3.2 ---")
-
-#String Lenght Cases
-def strLength(s):
-  if not s:
-    return 0
-
-  else:
-    return 1 + strLength(s[1:])
-
-output = strLength('abc')
-print(output)
-print("\n --- Problem 3.3 ---")
-#Recursive Digits Sum 
-def sumDigits(n):
-  if n == 0:
-    return 0
-  else:
-    return (n % 10) + sumDigits(n // 10)
-
-
-output = sumDigits(12345)
-print(output)
-print("\n --- Problem 3.4 ---")
-
-#Recursive Count 7s
-def countSevens(n):
-  if n == 0:
-    return 0
-  elif n % 10 == 7:
-    return 1 + countSevens(n // 10)
-  else:
-    return countSevens(n // 10)
-n = 727
-output = countSevens(n)
-print(output)
-print("\n --- Problem 3.5 ---")
-
-#Binary Search III
-def binaryIII(lst, target):
-  left, right = 0, len(lst) - 1
-
-  while left < right:
-    mid = (left + right) // 2
-
-    if lst[mid] == target:
-      return True
-    elif lst[mid] > target:
-      right = mid -1
-    else:
-      left = mid + 1
-
+  if paren[0] == "(" and paren[-1] == ')':
+    return isNested(paren[1:-1])
+  #if the question says that it doesnt mater where the ( ) pairs are along they are pairs then:
+  if paren[0] == ")" and paren[-1] == '(':
+    return isNested(paren[1:-1])
+  #if it doesnt meet up to the end, it will be false
   return False
 
-lst = lst = [1, 3, 5, 7, 9, 11, 13, 15]
-output = binaryIII(lst, 11)
-print(output)
-print("\n --- Problem 3.6 ---")
-#Find Missing
-def find_missing(nums):
-  left, right = 0, len(nums)
+input = '(()))'
+input2 = "(()" # False
+print(isNested(input))  
+print('\n --- Problem 1.2 ---')
+#How many 1s in a O(log n) time 
+def count_ones(lst):
+  #binary search 
+  low, high = 0, len(lst) - 1
 
-  while left < right:
-    mid = (left + right) // 2
-    if nums[mid] > mid:
-      right = mid
+  while low <= high:
+    mid = (low + high) // 2
+
+    if lst[mid] == 0:
+      low = mid + 1
     else:
-      left = mid + 1
-      
-  return left 
+      high = mid - 1
 
-nums = [0,1,2,3]
-output = find_missing(nums)
-print(output)
-print("\n --- Problem 3.7 ---")
-#Square Root 
-def sqrt(x):
-  if x < 2:
-    return X
-    
-  left, right = 1, x // 2
-  while left <= right:
-    mid = (left + right) // 2
-    mid_sqrt = mid * mid
-    if mid_sqrt == x:
-      return mid
-    elif mid_sqrt < x:
-      left = mid + 1
-    else:
-      right = mid - 1
+  if low < len(lst) and lst[low] == 1:
+    return len(lst) - low
+  return 0 
 
-  return right
+lst = [0, 0, 1, 0, 1, 1]
+print(count_ones(lst))
+print('\n --- Problem 1.3 ---')
+#Binary Search IV 
+def binary_searchIV(nums, left, right, target ):
+  if left > right:
+    return -1 #The Target is not found
 
-print(sqrt(16))
-    
+  mid = (left + right) // 2
+  #Target found
+  if nums[mid] == target:
+    return mid
+  #move to the left
+  if target < nums[mid]:
+    return binary_searchIV(nums, left, mid -1, target)
+  #move to the right 
+  else:
+    return binary_searchIV(nums, mid + 1, right, target)
+#Recursion helper 
+def binary_recursion(nums, target):
 
+  return binary_searchIV(nums, 0, len(nums) -1, target)
 
-  
-  
-
+input = [1, 3, 5, 11, 7, 0, 13, 15]
+print(binary_recursion(input, 0))
+ 
